32a206e08950ac5211e7bd1e72bdbe5fa8fcb34b
Youssef Chakir <youssef.chakir@e-polytechnique.ma>
2024-12-27 22:45:19 +0100

    update cors

--- src/main/java/com/chakir/CarHaven/CarHavenApplication.java
+++ src/main/java/com/chakir/CarHaven/CarHavenApplication.java
@@ -13,8 +13,8 @@ public class CarHavenApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/")
-						.allowedOrigins("http://localhost:3000")
+				registry.addMapping("/**").allowedOrigins("*")
+
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
 			}
 		};
